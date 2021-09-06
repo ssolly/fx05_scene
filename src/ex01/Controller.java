a47a5d99353047fcaa9a1f03c974b674dce508d6
Choi.SY <sollly0829@gmail.com>
2021-09-06 09:58:07 +0900

    fx05.ex01 기본설정(씬빌더,컨트롤러,메인)

--- src/ex01/Controller.java
+++ src/ex01/Controller.java
@@ -1,5 +1,26 @@
 package ex01;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+
+public class Controller implements Initializable {
+
+	private Parent root;
+
+	public void setRoot(Parent root) {
+		this.root = root;
+	}
+	
+	public void viewProc() {
+		System.out.println("확인버튼 클릭");
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- src/ex01/MainClass.java
+++ src/ex01/MainClass.java
@@ -1,6 +1,9 @@
 package ex01;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class MainClass extends Application {
@@ -10,9 +13,17 @@ public class MainClass extends Application {
 	}
 
 	@Override
-	public void start(Stage arg0) throws Exception {
+	public void start(Stage primaryStage) throws Exception {
 
-		arg0.show();
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("eventTest.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		Controller ctl = loader.getController();
+		ctl.setRoot(root);
+		
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("fx05.ex01");
+		primaryStage.show();
 		
 	}
 }
